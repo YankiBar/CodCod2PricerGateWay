1612128b1020aca4ca7875046ea98a603affd2b8
YankiBaras <y8463834@gmail.com>
2024-07-25 17:30:49 +0300

    something

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,13 +1,20 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
-import { GatewayModule } from './gateway/gateway.module';
+import { ConfigModule } from '@nestjs/config';
+import { HttpModule } from '@nestjs/axios';
+import configuration from './config/configuration';
 import { CodcodModule } from './codcod/codcod.module';
 import { PricerModule } from './pricer/pricer.module';
+import { GatewayService } from './gateway/jobs/main-flow/main-flow.service';
 
 @Module({
-  imports: [GatewayModule, CodcodModule, PricerModule],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    ConfigModule.forRoot({
+      load: [configuration],
+    }),
+    HttpModule,
+    CodcodModule,
+    PricerModule,
+    GatewayService
+  ],
 })
 export class AppModule {}
