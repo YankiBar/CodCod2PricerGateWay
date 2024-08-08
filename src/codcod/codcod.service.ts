d9cc9ac5434d1488b8992893eeafe0c9efc5b3a2
bizerba <bizerba@BIZERBA-SRV.MAZONKOL.co.il>
2024-08-08 14:59:33 +0300

    try to test the codcod API

--- src/codcod/codcod.service.ts
+++ src/codcod/codcod.service.ts
@@ -3,64 +3,97 @@ import { HttpService } from '@nestjs/axios';
 import { ConfigService } from '@nestjs/config';
 import { firstValueFrom } from 'rxjs';
 
-
 @Injectable()
 export class CodcodService {
-
   private readonly logger = new Logger(CodcodService.name);
-  private readonly baseURL: string;
+  private readonly ContentBaseURL: string;
+  private readonly MediaBaseURL: string;
 
-  constructor(private readonly httpService: HttpService,
+  constructor(
+    private readonly httpService: HttpService,
     private readonly configService: ConfigService,
   ) {
-    this.baseURL = this.configService.get<string>('CODCOD_API_URL');
-    // this.username = this.configService.get<string>('CODCOD_API_USERNAME');
-    // this.password = this.configService.get<string>('CODCOD_API_PASSWORD');
+    this.ContentBaseURL = this.configService.get<string>('CODCOD_CONTENT_URL');
+    this.MediaBaseURL = this.configService.get<string>('CODCOD_MEDIA_URL');
   }
 
   async getUpdatedItems(lastUpdateTime: string, storeId: string): Promise<any> {
-    const url = `${this.baseURL}/getUpdatedItems`;
+    const url = `${this.ContentBaseURL}/getUpdatedItems`;
     try {
       const response = await firstValueFrom(
         this.httpService.get(url, {
           params: { dt: lastUpdateTime, storeId },
           responseType: 'arraybuffer',
-        })
-      )
-      return response.data;
+        }),
+      );
+      const responseData = JSON.parse(
+        Buffer.from(response.data).toString('utf-8'),
+      );
+      if (responseData?.data?.Items) {
+        responseData.data.Items.forEach((item: any, index: number) => {
+          console.log(`Item ${index}:`, JSON.stringify(item, null, 2));
+        });
+        return responseData.data.Items;
+
+      } else {
+        this.logger.warn('No Items were found in the response.');
+        return [];
+      }
     } catch (error: any) {
-      this.logger.error(`Error fetching sign: ${error.message}`, error.stack);
+      this.logger.error(
+        `Error fetching updated Items: ${error.message}`,
+        error.stack,
+      );
       throw error;
     }
   }
 
-
-  async getUpdatedPromos(lastUpdateTime: string, storeId: string): Promise<any> {
-    const url = `${this.baseURL}/getUpdatedPromos`;
+  async getUpdatedPromos(
+    lastUpdateTime: string,
+    storeId: string,
+  ): Promise<any> {
+    const url = `${this.ContentBaseURL}/getUpdatedPromos`;
     try {
       const response = await firstValueFrom(
         this.httpService.get(url, {
           params: { dt: lastUpdateTime, storeId },
           responseType: 'arraybuffer',
-        })
-      )
-      return response.data;
+        }),
+      );
+      const responseData = JSON.parse(
+        Buffer.from(response.data).toString('utf-8'),
+      );
+      // Log each item in the Items array
+      if (responseData?.data?.Items) {
+        responseData.data.Items.forEach((item: any, index: number) => {
+          console.log(`Item ${index}:`, JSON.stringify(item, null, 2));
+        });
+        return responseData.data;
+      } else {
+        this.logger.warn('No Promos were found in the response.');
+      }
     } catch (error: any) {
-      this.logger.error(`Error fetching sign: ${error.message}`, error.stack);
+      this.logger.error(
+        `Error fetching Updated Promos: ${error.message}`,
+        error.stack,
+      );
       throw error;
     }
   }
 
-
-  async getItemSign(itemId: string, size: string, storeId: string): Promise<any> {
-    const url = `${this.baseURL}/getItemSign`;
+  async getItemSign(
+    itemId: string,
+    size: string,
+    storeId: string,
+  ): Promise<any> {
+    const url = `${this.MediaBaseURL}/getItemSign`;
     try {
       const response = await firstValueFrom(
         this.httpService.get(url, {
           params: { itemId, size, storeId },
           responseType: 'arraybuffer',
-        })
-      )
+        }),
+      );
       return response.data;
     } catch (error: any) {
       this.logger.error(`Error fetching sign: ${error.message}`, error.stack);
@@ -68,32 +101,35 @@ export class CodcodService {
     }
   }
 
-  async getPromoSign(promoNum: string, size: string, storeId: string): Promise<any> {
-    const url = `${this.baseURL}/getPromoSign`;
+  async getPromoSign(
+    promoNum: string,
+    size: string,
+    storeId: string,
+  ): Promise<any> {
+    const url = `${this.MediaBaseURL}/getPromoSign`;
     try {
       const response = await firstValueFrom(
         this.httpService.get(url, {
           params: { promoNum, size, storeId },
           responseType: 'arraybuffer',
-        })
-      )
+        }),
+      );
       return response.data;
     } catch (error: any) {
       this.logger.error(`Error fetching sign: ${error.message}`, error.stack);
       throw error;
     }
   }
-  
 
   async getSign(itemId: string, size: string, storeId: string): Promise<any> {
-    const url = `${this.baseURL}/getSign`;
+    const url = `${this.MediaBaseURL}/getSign`;
     try {
       const response = await firstValueFrom(
         this.httpService.get(url, {
           params: { itemId, size, storeId },
           responseType: 'arraybuffer',
-        })
-      )
+        }),
+      );
       return response.data;
     } catch (error: any) {
       this.logger.error(`Error fetching sign: ${error.message}`, error.stack);
@@ -101,20 +137,36 @@ export class CodcodService {
     }
   }
 
-
-  async fetchImage(itemId: string, size: string, storeId: string): Promise<Buffer> {
+  async fetchImage(
+    itemId: string,
+    size: string,
+    storeId: string,
+  ): Promise<Buffer> {
     let image: Buffer;
 
     if (itemId.startsWith('I')) {
-      const response = await this.getItemSign(itemId, size, storeId);
+      const response = await this.getItemSign(`${'I'}itemId`, size, storeId);
       image = Buffer.from(response.data);
     } else if (itemId.startsWith('P')) {
-      const response = await this.getPromoSign(itemId, size, storeId);
+      const response = await this.getPromoSign(`${'P'}itemId`, size, storeId);
       image = Buffer.from(response.data);
     } else {
       const response = await this.getSign(itemId, size, storeId);
       image = Buffer.from(response.data);
     }
+
+    return image;
+  }
+
+  async fetchItemImage(
+    itemId: string,
+    size: string,
+    storeId: string,
+  ): Promise<Buffer> {
+    let image: Buffer;
+
+    const response = await this.getItemSign(itemId, size, storeId);
+    image = Buffer.from(response.data);
 
     return image;
   }
--- src/gateway/jobs/main-flow/Gateway.service.ts
+++ src/gateway/jobs/main-flow/Gateway.service.ts
@@ -2,48 +2,101 @@ import { Injectable, Logger } from '@nestjs/common';
 import { Cron, CronExpression } from '@nestjs/schedule';
 import { CodcodService } from 'src/codcod/codcod.service';
 import { PricerService } from 'src/pricer/pricer.service';
+import AsciiArt from 'ascii-art';
+
+async function displayImageAsAscii(imageBuffer: Buffer) {
+  try {
+    const artwork = await AsciiArt.image({
+      data: imageBuffer,
+      width: 40, // Adjust the width as needed
+    });
+    console.log(artwork);
+  } catch (error) {
+    console.error('Failed to display image as ASCII:', error);
+  }
+}
+
+function addHoursToUtcTime(originalTime: string, hoursToAdd: number): string {
+  const date = new Date(originalTime);
+  date.setUTCHours(date.getUTCHours() + hoursToAdd);
+  return date.toISOString(); // Convert the Date object back to an ISO string
+}
 
 @Injectable()
 export class GatewayService {
   private readonly logger = new Logger(GatewayService.name);
-  private readonly storeId = process.env.STORE_ID;;
+  private readonly storeId = process.env.STORE_ID;
   private readonly projection = 'S';
 
   constructor(
     private readonly codcodService: CodcodService,
     private readonly pricerService: PricerService,
-  ) {
-  }
+  ) {}
 
   @Cron(CronExpression.EVERY_MINUTE)
   async processUpdates(): Promise<void> {
     const lastUpdateTime = new Date().toISOString();
-    console.log(`Processing updates since ${lastUpdateTime}`);
+    const updatedTime = addHoursToUtcTime(lastUpdateTime, 3);
+
+    console.log(`Processing updates since ${updatedTime}`);
 
     try {
       // Fetch updated items from Codcod
-      const codcodItems = await this.codcodService.getUpdatedItems(lastUpdateTime, this.storeId);
-      
-      // Fetch all labels from Pricer
-      const allLabels = await this.pricerService.getAllLabelsInStore(this.storeId);
-      
-      // Filter items that exist in both Codcod and Pricer
-      const pricerItemIds = allLabels.map(item => item.links.itemId);
-      const filteredItemIds = codcodItems.filter((id: any) => pricerItemIds.includes(id));
+      const codcodItems = await this.codcodService.getUpdatedItems(
+        lastUpdateTime,
+        this.storeId,
+      );
+      const codcodPromos = await this.codcodService.getUpdatedPromos(
+        lastUpdateTime,
+        this.storeId,
+      );
+      console.log(codcodItems);
+      if (Array.isArray(codcodItems) && codcodItems.length > 0) {
+        for (const item in codcodItems) {
+          this.logger.log(`Processing itemId: ${codcodItems[item].itemId}`);
+          // Fetch item image from Codcod
+          const image = await this.codcodService.fetchItemImage(
+            codcodItems[item].barcode,
+            '768X920',
+            this.storeId,
+          );
+          this.logger.log(`Image size: ${image.length} bytes`);
+          this.logger.log('Displaying image as ASCII...');
+          await displayImageAsAscii(image);  
+
+          if (!image) {
+            this.logger.warn(
+              `No image found for itemId: ${codcodItems[item].itemId}`,
+            );
+            continue;
+          }
+        }
+      }
+      // // Fetch all labels from Pricer
+      // const allLabels = await this.pricerService.getAllLabelsInStore(this.storeId);
+
+      // // Filter items that exist in both Codcod and Pricer
+      // const pricerItemIds = allLabels.map(item => item.links.itemId);
+      // const filteredItemIds = codcodItems.filter((id: any) => pricerItemIds.includes(id));
 
       // Process the filtered items
-      await this.processItems(filteredItemIds);
+      // await this.processItems(filteredItemIds);
 
       this.logger.log('Processing completed.');
     } catch (error) {
       this.logger.error('Error processing updates:', error.stack);
     }
   }
+
   async processItems(itemIds: string[]): Promise<void> {
     for (const itemId of itemIds) {
       try {
         // Fetch item image from Codcod
-        const image = await this.codcodService.fetchImage(itemId, 'defaultSize', this.storeId);
+        const image = await this.codcodService.fetchImage(
+          itemId,
+          'defaultSize',
+          this.storeId,
+        );
         if (!image) {
           this.logger.warn(`No image found for itemId: ${itemId}`);
           continue;
